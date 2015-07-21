d775912203c4ece80d997ae24212aceea29f00d7
dma-wiremaze <domingos.araujo@wiremaze.com>
2015-07-21 16:09:06 +0000

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -1,8 +1,11 @@
 source 'https://rubygems.org'
 
 gem 'rails',        '4.2.2'
-gem 'bootstrap-sass',       '3.2.0.0'
+gem 'bootstrap-sass',          '3.2.0.0'
 gem 'bcrypt',       '3.1.7'
+gem 'faker',        '1.4.2'
+gem 'will_paginate',           '3.0.7'
+gem 'bootstrap-will_paginate', '0.0.10'
 gem 'sass-rails',   '5.0.2'
 gem 'uglifier',     '2.5.3'
 gem 'coffee-rails', '4.1.0'
@@ -28,5 +31,4 @@ group :production do
   gem 'pg',             '0.17.1'
   gem 'rails_12factor', '0.0.2'
   gem 'puma',           '2.11.1'
-
 end
--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -10,6 +10,10 @@ module SessionHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
   
+  def current_user?(user)
+    user == current_user
+  end
+  
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
@@ -26,11 +30,6 @@ module SessionHelper
     !current_user.nil?
   end
   
-  def log_out
-    session.delete(:user_id)
-    @current_user = nil
-  end
-  
   def forget(user)
     user.forget
     cookies.delete(:user_id)
@@ -41,6 +40,15 @@ module SessionHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+  
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
   end
   
 end

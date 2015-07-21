38b218a6f834db8d510bde308b5e033aa1ca3fae
dma-wiremaze <domingos.araujo@wiremaze.com>
2015-07-21 10:01:02 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  
-  get 'usres/new'
 
   get 'users/new'
 
@@ -9,6 +7,7 @@ Rails.application.routes.draw do
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
+  resources :users
 
 
   
